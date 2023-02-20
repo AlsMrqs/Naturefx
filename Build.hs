aa359dd53c731a526ce1d13df12ff3827fce3d19
naturefx <alsmrqs@outlook.com>
2023-02-20 09:59:31 -0300

    instance-functor-tree

--- Build.hs
+++ Build.hs
@@ -3,8 +3,14 @@ module Build where
 import System.Environment
 import System.IO
 import Data.List
+import Data.Foldable
 
-data Tree a = Leaf a | Node a (Tree a) (Tree a)
+data Tree a = Root | Leaf a | Node a (Tree a) (Tree a)
+
+instance Functor Tree where
+    fmap f (Node x l r) = Node (f x) (fmap f l) (fmap f r)
+    fmap f (Leaf a) = Leaf (f a)
+    fmap f _ = Root
 
 -- Data.Base --
 html = intercalate "\n" source
