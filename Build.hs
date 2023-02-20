7c4e8211fa8b0d8db3c4a1da48f4f8b7cf5ccf1d
naturefx <alsmrqs@outlook.com>
2023-02-20 09:34:29 -0300

    data Tree

--- Build.hs
+++ Build.hs
@@ -4,15 +4,20 @@ import System.Environment
 import System.IO
 import Data.List
 
+data Tree a = Leaf a | Node a (Tree a) (Tree a)
+
+-- Data.Base --
 html = intercalate "\n" source
 source = 
     ["<!DOCTYPE html>"
     ,"<html>"
-    ,"<head>"
-    ,"<title>","</title>"
-    ,"</head>"
+    ,"<head>" ,"<title>","</title>" ,"</head>"
     ,"<body>","</body>"
     ,"</html>"]
+-- End --
 
 genesis = getArgs >>= mapM_ (`System.IO.writeFile` html)
 
+-- Todo --
+-- tree :: (Foldable f) => f [Char] -> Tree [Char]
+-- seed :: [Char] -> Tree [Char]
