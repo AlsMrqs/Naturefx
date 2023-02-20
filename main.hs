aa0693811b344389b6d8db516d86b5ae9c5f0ff6
naturefx <alsmrqs@outlook.com>
2023-02-20 15:34:35 -0300

    calling-page

--- main.hs
+++ main.hs
@@ -1,12 +1,9 @@
-import System.Directory
-import System.Environment
-import System.IO.Error
-import System.IO
-
-import Build
+import Kagepr
 
 main :: IO ()
 main = do
-    genesis
-    putStr $ (++"\n") "lambda-x!"
 
+
+
+
+putStr _page_
